111c06818cade5a86459691b730bf190618a649a
Ken Rockot <ken@oz.gs>
2013-07-25 07:13:12 -0700

    New buffers get randomized names now

--- src/edit/buffer.js
+++ src/edit/buffer.js
@@ -4,11 +4,19 @@ cmacs.edit.Buffer = function(opt_options) {
   var contents = options.contents || '';
 
   this.mode_ = options.mode || 'text';
-  this.name_ = options.name || 'new';
+  this.name_ = options.name || this.generateRandomName_();
   this.document_ = new ace.Document(contents);
 
   this.onChanged = new cmacs.common.Event();
   this.onTitleChanged = new cmacs.common.Event();
+};
+
+cmacs.edit.Buffer.prototype.generateRandomName_ = function() {
+  var name = '';
+  for (var i = 0; i < 8; ++i) {
+    name += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  }
+  return name;
 };
 
 cmacs.edit.Buffer.prototype.setName = function(name) {
